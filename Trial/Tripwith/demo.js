24f8a0468473348d0c5c034ebda0b6af5827923a
hielynn <enk4127@gmail.com>
2021-11-28 00:35:17 +0800

    t

---
+++ Trial/Tripwith/demo.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+	$('body > input').eq(0).on('keyup', function() {
+		let inputVal = $(this).val(); // val() = value
+		let textarea = $('body > textarea').eq(0);
+		textarea.html(inputVal);
+	});
+};
